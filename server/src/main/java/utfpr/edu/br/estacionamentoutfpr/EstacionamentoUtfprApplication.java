1ff79b3d97c467666a14d196a1d3c77ce14c45ee
MatheusBrunetto <brunetto@bitzsoftwares.com.br>
2022-09-01 20:25:59 -0300

    Arquivos iniciais do projeto

---
+++ server/src/main/java/utfpr/edu/br/estacionamentoutfpr/EstacionamentoUtfprApplication.java
@@ -0,0 +1,13 @@
+package utfpr.edu.br.estacionamentoutfpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoUtfprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentoUtfprApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/utfpr/edu/br/estacionamentoutfpr/EstacionamentoUtfprApplicationTests.java
@@ -0,0 +1,13 @@
+package utfpr.edu.br.estacionamentoutfpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoUtfprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
